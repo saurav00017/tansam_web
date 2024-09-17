33af825378074ce6aba5b6818c14e13e0159fa73
Prabu <prabu.tansam@gmail.com>
2024-09-17 12:25:07 +0530

    calendar functionality

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from app import db
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    description = db.Column(db.String(200))
